c0568ff62585efc70f06b3f5131a60830b27f8fe
JoseZelada6 <2023-40054@liceocanadiense.edu.gt>
2024-06-06 14:12:02 -0600

    se subira 3 proyectos

---
+++ eje1.py
@@ -0,0 +1,17 @@
+
+
+a = 5
+b = 6
+c = 10
+d = 20 
+e = 7 
+
+suma = a + b + c + d + e
+resta = a - b - c - d - e
+mul = a * b * c * d * e
+div = a / b / c / d / e
+
+print ("la suma de los 5 dijitos es de ", suma )
+print ("la resta de los 5 dijitos es de ", resta )
+print ("la multiplicacion de los 5 dijitos es de ", mul )
+print (f"la division de los 5 dijitos es de  { div: .2f}"  )
