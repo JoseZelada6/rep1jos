52b51a8f2757b0de9982f29be04e08aa463c0afd
JoseZelada6 <2023-40054@liceocanadiense.edu.gt>
2024-06-07 12:29:35 -0600

    Ejercicio 2 H1 U3

---
+++ eje2.py
@@ -0,0 +1,15 @@
+n1 = int(input("ingresar el primer dato \n"))
+n2 = int(input("ingresar el segundo dato \n"))
+n3 = int(input("ingresar el tercer dato \n"))
+n4 = int(input("ingresar el cuerto dato \n"))
+n5 = int(input("ingresar el quinto dato \n"))
+
+
+suma = n1 + n2 + n3 + n4 + n5
+resta = n1 - n2 - n3 - n4 - n5
+multiplicacion = n1 * n2 * n3 * n4 * n5
+divicion = n1 / n2 / n3 / n4 / n5
+print (f"la suma de los datos es {suma: .2f}")
+print (f"la resta de los datos es {resta: .2f}")
+print (f"la multiplicacion de los datos es {multiplicacion: .2f}")
+print (f"la divicion de los datos es {divicion: .2f}")
